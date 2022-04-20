68bce9bc367638f204073feae45eeac2bf1079fc
Kateřina Baňacká <banackakaterina@in-ius.cz>
2022-04-20 15:31:17 +0200

    Snaha o zprovoznení JS

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-/* const tahKolecko = 'circle';
-const btnElm = document.querySelectorAll('.btn');
-
-btnElm.addEventListener('click', (event) => {
-  event.preventDefault();
-  event.target.classList.add('board__field--circle');
-}); */
-
+const buttons = document.querySelectorAll('button');
 const tahKolecko = (event) => {
-  event.target.classList.add('board__field--circle');
+  event.target.classList.add('gameArea--circle');
 };
 
-const buttons = document.querySelectorAll('.btn');
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', tahKolecko);
 }
